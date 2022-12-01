a7eabeaab37b075107de6890f43b5534cba4d29c
Manny <etetopat@student.42bangkok.com>
2022-12-02 05:29:31 +0700

    linux version added

---
+++ common_core/04_so_long_linux/src/mandatory/moves.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 00:55:43 by Manny             #+#    #+#             */
+/*   Updated: 2022/12/02 05:25:20 by Manny            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../so_long.h"
+
+static void	change_coordinates(int keycode, t_player_position *old,
+								t_player_position *new)
+{
+	if (keycode == ARROW_UP)
+	{
+		new->x = old->x;
+		new->y = old->y - 1;
+	}
+	if (keycode == ARROW_LEFT)
+	{
+		new->x = old->x - 1;
+		new->y = old->y;
+	}
+	if (keycode == ARROW_DOWN)
+	{
+		new->x = old->x;
+		new->y = old->y + 1;
+	}
+	if (keycode == ARROW_RIGHT)
+	{
+		new->x = old->x + 1;
+		new->y = old->y;
+	}
+}
+
+char	make_player_moves(t_map *map, int keycode, int exit)
+{
+	t_player_position	old;
+	t_player_position	new;
+	char				passed;
+
+	old.y = map->player_position.y;
+	old.x = map->player_position.x;
+	change_coordinates(keycode, &old, &new);
+	passed = '1';
+	if (map->map_data[new.y][new.x] != '1')
+	{
+		passed = map->map_data[new.y][new.x];
+		map->map_data[new.y][new.x] = 'P';
+		map->map_data[old.y][old.x] = '0';
+	}
+	if (exit == FALSE && passed == 'E')
+	{
+		map->map_data[old.y][old.x] = 'P';
+		map->map_data[new.y][new.x] = 'E';
+	}
+	return (passed);
+}
+
+int	game_action(int keycode, t_game *game)
+{
+	char	passed;
+
+	if (keycode == KEY_ESC)
+		game_close(game);
+	if (keycode != ARROW_UP && keycode != ARROW_LEFT && keycode != ARROW_DOWN
+		&& keycode != ARROW_RIGHT)
+		return (0);
+	if (game->map.items_to_collect == 0)
+		game->exit = TRUE;
+	passed = make_player_moves(&(game->map), keycode, game->exit);
+	if (passed != '1' && !(game->exit == FALSE && passed == 'E'))
+	{
+		ft_putstr_fd("Number of moves: ", 0);
+		ft_putnbr_fd(++(game->steps), 1);
+		ft_putstr_fd("\n", 1);
+	}
+	if (passed == 'E' && game->map.items_to_collect == 0)
+		game_win(game);
+	img_to_win(game);
+	return (0);
+}
---
+++ common_core/04_so_long_linux/src/map_init.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_init.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 11:33:43 by Manny             #+#    #+#             */
+/*   Updated: 2022/11/30 20:01:53 by Manny            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+static char	*skip_beginning_newlines(int fd)
+{
+	char	*line;
+
+	line = get_next_line(fd);
+	while (line != NULL && line[0] == '\n')
+	{
+		free(line);
+		line = get_next_line(fd);
+	}
+	return (line);
+}
+
+static void	skip_ending_newlines(int fd)
+{
+	char	*line;
+
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		if (ft_strlen(line, 1) > 0)
+		{
+			free(line);
+			close(fd);
+			display_error_exit(MAP_SIZE_ERR);
+		}
+		line = get_next_line(fd);
+	}
+}
+
+void	map_size_init(t_map *map, char *ber)
+{
+	t_file	file;
+
+	map->height = 0;
+	file.fd = open(ber, O_RDONLY);
+	file.line = skip_beginning_newlines(file.fd);
+	while (file.line != NULL)
+	{
+		map->width = ft_strlen(file.line, 1);
+		free(file.line);
+		(map->height)++;
+		file.line = get_next_line(file.fd);
+		if (file.line != NULL && map->width != ft_strlen(file.line, 1))
+		{
+			free(file.line);
+			display_error_exit(MAP_SIZE_ERR);
+		}
+		if (file.line != NULL && file.line[0] == '\n')
+			break ;
+	}
+	skip_ending_newlines(file.fd);
+	close(file.fd);
+	map->map_data = (char **)malloc(sizeof(char *) * map->height);
+	if (map->map_data == NULL)
+		display_error_exit(MALLOC_ERR);
+}
+
+void	map_init(t_map *map, char *ber)
+{
+	t_file	file;
+	int		height;
+
+	is_ber_file(ber);
+	map_size_init(map, ber);
+	height = 0;
+	file.fd = open(ber, O_RDONLY);
+	file.line = skip_beginning_newlines(file.fd);
+	if (file.line == NULL)
+		display_error_exit(OTHER_ERR);
+	while (height < map->height)
+	{
+		map->map_data[height] = (char *)malloc(sizeof(char) * (map->width + 2));
+		if (map->map_data[height] == NULL)
+			display_error_exit(MALLOC_ERR);
+		ft_strcpy(map->map_data[height], file.line);
+		map->map_data[height][map->width] = '\0';
+		free(file.line);
+		height++;
+		file.line = get_next_line(file.fd);
+	}
+	close(file.fd);
+	map_symbols_checker(map);
+}
