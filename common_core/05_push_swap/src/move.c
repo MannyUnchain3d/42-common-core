05a10893ad7a74be11dc488b177218f6307c7ad4
Manny <etetopat@student.42bangkok.com>
2022-12-26 15:56:06 +0700

    sorting algo added

--- common_core/05_push_swap/src/move.c
+++ common_core/05_push_swap/src/move.c
@@ -6,8 +6,87 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 22:40:55 by Manny             #+#    #+#             */
-/*   Updated: 2022/12/22 22:41:12 by Manny            ###   ########.fr       */
+/*   Updated: 2022/12/26 14:25:41 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+/* Rotates stack A and B until one of the stack is in position,
+ * the cost is positive because numbers are in the top half of the stack,
+ * the cost is decremented until one reaches 0 */
+static void	rotate_both(t_stack **a, t_stack **b, int *cost_a, int *cost_b)
+{
+	while (*cost_a > 0 && *cost_b > 0)
+	{
+		(*cost_a)--;
+		(*cost_b)--;
+		rr(a, b);
+	}
+}
+
+/* Reverse rotates stack A and B until one of the stack is in position,
+ * the cost is negative because numbers are in the bottom half of the stack,
+ * the cost is incremented until one reaches 0 */
+static void	rev_rotate_both(t_stack **a, t_stack **b, int *cost_a, int *cost_b)
+{
+	while (*cost_a < 0 && *cost_b < 0)
+	{
+		(*cost_a)++;
+		(*cost_b)++;
+		rrr(a, b);
+	}
+}
+
+/* Rotates stack A until it is in position, if the cost is positive,
+ * the stack will be rotated, if it is negative, it will be reverse rotated */
+static void	rotate_a(t_stack **a, int *cost)
+{
+	while (*cost)
+	{
+		if (*cost > 0)
+		{
+			ra(a);
+			(*cost)--;
+		}
+		else
+		{
+			rra(a);
+			(*cost)++;
+		}
+	}
+}
+
+/* Rotates stack B until it is in position, if the cost is positive,
+ * the stack will be rotated, if it is negative, it will be reverse rotated */
+static void	rotate_b(t_stack **b, int *cost)
+{
+	while (*cost)
+	{
+		if (*cost > 0)
+		{
+			rb(b);
+			(*cost)--;
+		}
+		else
+		{
+			rrb(b);
+			(*cost)++;
+		}
+	}
+}
+
+/* Rotates or reverse rotates stack A and B until they are in position,
+ * if the cost is the same, both stacks will be rotated or reverse rotated
+ * at the same time, if the cost is different, it will be done separately
+ * for each stack */
+void	move(t_stack **a, t_stack **b, int cost_a, int cost_b)
+{
+	if (cost_a > 0 && cost_b > 0)
+		rotate_both(a, b, &cost_a, &cost_b);
+	else if (cost_a < 0 && cost_b < 0)
+		rev_rotate_both(a, b, &cost_a, &cost_b);
+	rotate_a(a, &cost_a);
+	rotate_b(b, &cost_b);
+	pb(a, b);
+}
--- common_core/05_push_swap/src/sort.c
+++ common_core/05_push_swap/src/sort.c
@@ -6,8 +6,82 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 18:42:55 by Manny             #+#    #+#             */
-/*   Updated: 2022/12/22 20:35:14 by Manny            ###   ########.fr       */
+/*   Updated: 2022/12/26 15:52:40 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+/* Pushes all the numbers of stack A into stack B, until there is only 3 numbers
+ * left in stack A. Pushes the smaller numbers first to improve efficiency */
+void	push_all_but_3(t_stack **stack_a, t_stack **stack_b)
+{
+	int	stack_size;
+	int	pushed;
+	int	i;
+
+	stack_size = stack_find_size(*stack_a);
+	pushed = 0;
+	i = 0;
+	while (stack_size > 6 && i < stack_size && pushed < stack_size / 2)
+	{
+		if ((*stack_a)->index <= stack_size / 2)
+		{
+			pb(stack_a, stack_b);
+			pushed++;
+		}
+		else
+			ra(stack_a);
+		i++;
+	}
+	while (stack_size - pushed > 3)
+	{
+		pb(stack_a, stack_b);
+		pushed++;
+	}
+}
+
+/* Shifts stack A until the smallest number is at the top */
+static void	shift_stack(t_stack **stack_a)
+{
+	int	lowest_index_pos;
+	int	stack_size;
+
+	lowest_index_pos = find_lowest_index_pos(*stack_a);
+	stack_size = stack_find_size(*stack_a);
+	if (lowest_index_pos > stack_size / 2)
+	{
+		while (lowest_index_pos < stack_size)
+		{
+			rra(stack_a);
+			lowest_index_pos++;
+		}
+	}
+	else
+	{
+		while (lowest_index_pos > 0)
+		{
+			ra(stack_a);
+			lowest_index_pos--;
+		}
+	}
+}
+
+/* Sorting algorithm for more than 3 numbers in a stack.
+ * Pushes all  numbers from stack A to stack B except for the last 3 numbers.
+ * Sorts the last 3 numbers in stack A using sort_3nb.
+ * Pushes the numbers back to stack A by calculating the best cost-efficient way.
+ * Shifts stack A until the smallest number is at the top */
+void	sort(t_stack **stack_a, t_stack **stack_b)
+{
+	push_all_but_3(stack_a, stack_b);
+	sort_3nb(stack_a);
+	while (*stack_b)
+	{
+		find_target_pos(stack_a, stack_b);
+		find_cost(stack_a, stack_b);
+		cheapest_move(stack_a, stack_b);
+	}
+	if (!is_sorted(*stack_a))
+		shift_stack(stack_a);
+}
