6ec71d8753d5216806b7d2502192683285262f7c
Manny <etetopat@student.42bangkok.com>
2022-12-26 21:52:50 +0700

    input_checked_utils renamed

--- common_core/05_push_swap/src/input_check.c
+++ common_core/05_push_swap/src/input_check.c
@@ -6,7 +6,7 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 00:07:05 by Manny             #+#    #+#             */
-/*   Updated: 2022/12/26 20:01:10 by Manny            ###   ########.fr       */
+/*   Updated: 2022/12/26 21:28:31 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,10 @@ static int	arg_is_duplicate(char **argv)
 	int	j;
 
 	i = 1;
-	while (argv[i] != '\0')
+	while (argv[i])
 	{
 		j = 1;
-		while (argv[j] != '\0')
+		while (argv[j])
 		{
 			if (j != i && strnb_cmp(argv[i], argv[j]) == 0)
 				return (1);
@@ -72,7 +72,7 @@ int	is_valid_input(char **argv)
 
 	i = 1;
 	zeros = 0;
-	while (argv[i] != '\0')
+	while (argv[i])
 	{
 		if (!arg_is_number(argv[i]))
 			return (0);
