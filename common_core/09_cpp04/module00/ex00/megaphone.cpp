fc38fb79b4ef5431120bef5a6b19194e9af37648
Manny <etetopat@student.42bangkok.com>
2023-07-03 14:11:42 +0700

    initial commit

--- common_core/09_cpp04/module00/ex00/megaphone.cpp
+++ common_core/09_cpp04/module00/ex00/megaphone.cpp
@@ -6,14 +6,25 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 02:24:32 by Manny             #+#    #+#             */
-/*   Updated: 2023/07/03 03:09:09 by Manny            ###   ########.fr       */
+/*   Updated: 2023/07/03 13:01:26 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main()
+int	main(int ac, char **av)
 {
-	std::cout << "Hello World" << std::endl;
-	return 0;
+	std::string const msg = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	
+	if (ac == 1)
+		std::cout << msg;
+	for (int i = 1; av[i]; i++)
+	{
+		std::string s = av[i];
+		for (std::string::iterator it = s.begin(); it != s.end(); it++)
+			*it = std::toupper(*it);
+		std::cout << s;
+	}
+	std::cout << std::endl;
+	return (0);
 }
