f88ba20c864bfc62904ba7dacb9a7768608abd5b
Manny <etetopat@student.42bangkok.com>
2023-07-05 04:32:31 +0700

    timestamp method added

--- common_core/09_cpp04/module00/ex02/Account.cpp
+++ common_core/09_cpp04/module00/ex02/Account.cpp
@@ -6,11 +6,12 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 02:21:14 by Manny             #+#    #+#             */
-/*   Updated: 2023/07/05 02:44:48 by Manny            ###   ########.fr       */
+/*   Updated: 2023/07/05 04:31:56 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 #include "Account.hpp"
 
@@ -22,12 +23,67 @@ int	Account::_totalNbWithdrawals = 0;
 
 /* ----- CONSTRUCTOR ------------------ */
 
+Account::Account(int initial_deposit) : _accountIndex(_nbAccounts),
+										_amount(initial_deposit),
+										_nbDeposits(0),
+										_nbWithdrawals(0) {
+	Account::_nbAccounts++;
+	Account::_totalAmount += _amount;
+	_displayTimestamp();
+	std::cout 	<< "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";"
+				<< "created" << std::endl;
+	return ;
+}
+
 /* ----- DESTRUCTOR ------------------- */
+
+Account::~Account(void) {
+	Account::_nbAccounts--;
+	_displayTimestamp();
+	std::cout 	<< "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";"
+				<< "closed" << std::endl;
+	return ;
+}
 
 /* ----- GETTERS ---------------------- */
 
-/* ----- SETTERS ---------------------- */
+/* Returns the number of existing accounts */
+int	Account::getNbAccounts(void) {
+	return (Account::_nbAccounts);
+}
+
+/* Returns the total amount in all accounts */
+int	Account::getTotalAmount(void) {
+	return (Account::_totalAmount);
+}
+
+/* Returns the total number of deposits */
+int	Account::getNbDeposits(void) {
+	return (Account::_totalNbDeposits);
+}
+
+/* Returns the total number of withdrawals */
+int	Account::getNbWithdrawals(void) {
+	return (Account::_totalNbWithdrawals);
+}
 
 /* ----- PRIVATE METHODS -------------- */
 
+/* Displays the current date and time followed by a space.
+ * Format: [YYYYMMDD_HHMMSS] */
+void	Account::_displayTimestamp(void) {
+	std::time_t	t = std::time(NULL);
+	std::tm		*now = std::localtime(&t);
+
+	std::cout << std::setfill('0') << "[" << now->tm_year + 1900
+			  << std::setw(2) << now->tm_mon + 1
+			  << std::setw(2) << now->tm_mday << "_"
+			  << std::setw(2) << now->tm_hour
+			  << std::setw(2) << now->tm_min
+			  << std::setw(2) << now->tm_sec << "] " << std::flush;
+}
+
 /* ----- PUBLIC METHODS --------------- */
+
