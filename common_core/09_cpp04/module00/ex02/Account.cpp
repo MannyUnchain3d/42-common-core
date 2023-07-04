dde91b87269aa7d9cfa4128ace011ec572503c3a
Manny <etetopat@student.42bangkok.com>
2023-07-05 02:45:24 +0700

    initial commit

---
+++ common_core/09_cpp04/module00/ex02/Account.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 02:21:14 by Manny             #+#    #+#             */
+/*   Updated: 2023/07/05 02:44:48 by Manny            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctime>
+#include "Account.hpp"
+
+/* Static attributes initialization */
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+/* ----- CONSTRUCTOR ------------------ */
+
+/* ----- DESTRUCTOR ------------------- */
+
+/* ----- GETTERS ---------------------- */
+
+/* ----- SETTERS ---------------------- */
+
+/* ----- PRIVATE METHODS -------------- */
+
+/* ----- PUBLIC METHODS --------------- */
