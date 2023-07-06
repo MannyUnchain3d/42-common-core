3a1904505d2818c60f2ff5c22c77f08ca22d7258
Emmanuel Tetopata <etetopat@student.42bangkok.com>
2023-07-07 00:38:24 +0700

    colors added

--- common_core/09_cpp04/module01/ex00/inc/Zombie.hpp
+++ common_core/09_cpp04/module01/ex00/inc/Zombie.hpp
@@ -6,25 +6,36 @@
 /*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 18:07:12 by etetopat          #+#    #+#             */
-/*   Updated: 2023/07/06 19:25:53 by etetopat         ###   ########.fr       */
+/*   Updated: 2023/07/07 00:26:00 by etetopat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
-# include <string>
+# include <iostream>
+
+# define RED		"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define YELLOW		"\033[0;33m"
+# define BLUE		"\033[0;34m"
+# define MAGENTA	"\033[0;35m"
+# define CYAN		"\033[0;36m"
+# define BOLD		"\033[1m"
+# define ITALIC		"\033[3m"
+# define UNDERLINE	"\033[4m"
+# define BLINK		"\033[5m"
+# define NC			"\033[0m"
 
 class Zombie {
 	public:
-		Zombie(void);
 		Zombie(std::string name);
 		~Zombie(void);
 		void	announce(void) const;
 
 	private:
 		std::string	_name;
-		std::string _getName(void) const;
+		std::string	_getName(void) const;
 };
 
 Zombie* newZombie(std::string name);
