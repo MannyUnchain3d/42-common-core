97d1feb778dc1fc4444596c9716fed914ad80112
Manny <etetopat@student.42bangkok.com>
2023-07-09 03:18:11 +0700

    initial commit

---
+++ common_core/10_cpp04/module01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 03:06:44 by Manny             #+#    #+#             */
+/*   Updated: 2023/07/09 03:16:15 by Manny            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+ int	main(void) {
+ 	std::string const	str = "HI THIS IS BRAIN";
+	std::string const*	stringPTR = &str;
+	std::string const&	stringREF = str;
+
+	std::cout << "Address of str: " << &str << std::endl;
+	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+	return (0);
+ }
