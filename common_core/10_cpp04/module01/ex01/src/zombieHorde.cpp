38ce9ea010a59195b4a2a23100705ed00a661045
Manny <etetopat@student.42bangkok.com>
2023-07-09 02:40:46 +0700

    coding style changed

--- common_core/10_cpp04/module01/ex01/src/zombieHorde.cpp
+++ common_core/10_cpp04/module01/ex01/src/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/08 01:46:35 by Manny             #+#    #+#             */
-/*   Updated: 2023/07/09 02:21:19 by Manny            ###   ########.fr       */
+/*   Updated: 2023/07/09 02:37:45 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 Zombie*	zombieHorde(int N, std::string name) {
 	if (N <= 0)
 		return (NULL);
-	Zombie* zombies;
-	zombies = new Zombie[N];
+	Zombie* zombies = new Zombie[N];
 	for (int i = 0; i < N; i++)
 		zombies[i].setName(name);
 	return (zombies);
