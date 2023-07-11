ac059a85d4a8dc12d5466b69835d7eca1c634825
Manny <etetopat@student.42bangkok.com>
2023-07-12 02:18:07 +0700

    colors added

--- common_core/00_Templates/Sample.hpp
+++ common_core/00_Templates/Sample.hpp
@@ -1,6 +1,17 @@
 #ifndef __SAMPLE_HPP__
 # define __SAMPLE_HPP__
 
+# define RED		"\033[0;31m"
+# define GREEN		"\033[0;32m"
+# define YELLOW		"\033[0;33m"
+# define BLUE		"\033[0;34m"
+# define MAGENTA	"\033[0;35m"
+# define CYAN		"\033[0;36m"
+# define BOLD		"\033[1m"
+# define ITALIC		"\033[3m"
+# define UNDERLINE	"\033[4m"
+# define BLINK		"\033[5m"
+# define NC			"\033[0m"
 class Sample {
 	public:
 		Sample(void); // Class Constructor
