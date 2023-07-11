2ef97ec0e59d9891fa3309ced5e699ca0ad4f806
Manny <etetopat@student.42bangkok.com>
2023-07-12 02:31:00 +0700

    messages simplified

--- common_core/10_cpp04/module01/ex05/Harl.cpp
+++ common_core/10_cpp04/module01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 02:08:19 by Manny             #+#    #+#             */
-/*   Updated: 2023/07/12 02:07:43 by Manny            ###   ########.fr       */
+/*   Updated: 2023/07/12 02:29:57 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,17 @@ void	Harl::complain(std::string level) {
 }
 
 void	Harl::debug(void) {
-	std::cout << YELLOW << "I love having extra bacon for my 7XL-double-cheese-triple-pickle",
-	std::cout << "-special ketchup burger. I really do!" << NC << std::endl;
+	std::cout << YELLOW << "I love having extra vegan bacon in my burger!" << NC << std::endl;
 }
 
 void	Harl::info(void) {
-	std::cout << YELLOW << "I cannot believe adding extra bacon costs more money. ",
-	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << NC << std::endl;
+	std::cout << YELLOW << "You didn’t put enough vegan bacon in my burger!" << NC << std::endl;
 }
 
 void	Harl::warning(void) {
-	std::cout << YELLOW << "I think I deserve to have some extra bacon for free. ",
-	std::cout << "I’ve been coming for years whereas you started working here since last month." << NC << std::endl;
+	std::cout << YELLOW << "I deserve to have some extra vegan bacon for free!" << NC << std::endl;
 }
 
 void	Harl::error(void) {
-	std::cout << YELLOW << "This is unacceptable! I want to speak to the manager now." << NC << std::endl;
+	std::cout << YELLOW << "This is unacceptable! I want to speak to the manager now!" << NC << std::endl;
 }
