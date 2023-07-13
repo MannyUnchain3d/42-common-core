cde746de71ba1f87bbb2ff8a4a3377e5a7035053
Emmanuel Tetopata <etetopat@student.42bangkok.com>
2023-07-13 22:21:49 +0700

    style changed

--- common_core/10_cpp04/module02/ex00/Fixed.hpp
+++ common_core/10_cpp04/module02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 20:08:38 by etetopat          #+#    #+#             */
-/*   Updated: 2023/07/13 20:42:47 by etetopat         ###   ########.fr       */
+/*   Updated: 2023/07/13 21:09:14 by etetopat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,6 @@
 # define FIXED_HPP
 
 # include <iostream>
-
-# define RED		"\033[0;31m"
-# define GREEN		"\033[0;32m"
-# define YELLOW		"\033[0;33m"
-# define BLUE		"\033[0;34m"
-# define MAGENTA	"\033[0;35m"
-# define CYAN		"\033[0;36m"
-# define BOLD		"\033[1m"
-# define ITALIC		"\033[3m"
-# define UNDERLINE	"\033[4m"
-# define BLINK		"\033[5m"
-# define NC			"\033[0m"
 class Fixed {
 	public:
 		Fixed(void);
