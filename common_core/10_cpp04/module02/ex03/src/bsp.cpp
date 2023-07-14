e31325f61fbfb4c6069549da45b045e773dac6b3
Emmanuel Tetopata <etetopat@student.42bangkok.com>
2023-07-15 02:29:46 +0700

    test bsp

--- common_core/10_cpp04/module02/ex03/src/bsp.cpp
+++ common_core/10_cpp04/module02/ex03/src/bsp.cpp
@@ -6,17 +6,17 @@
 /*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 18:33:34 by etetopat          #+#    #+#             */
-/*   Updated: 2023/07/14 19:20:10 by etetopat         ###   ########.fr       */
+/*   Updated: 2023/07/15 02:19:54 by etetopat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-static Fixed	abs(Fixed x) {
+Fixed	abs(Fixed x) {
 	return (x < 0 ? x * -1 : x);
 }
 
-static Fixed	area(Point const a, Point const b, Point const c) {
+Fixed	area(Point const a, Point const b, Point const c) {
 	return (((a.getX() * (b.getY() - c.getY()))
 			+ (b.getX() * (c.getY() - a.getY()))
 			+ (c.getX() * (a.getY() - b.getY()))) / 2);
@@ -28,5 +28,43 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point) {
 	Fixed	area2 = abs(area(point, b, c));
 	Fixed	area3 = abs(area(point, c, a));
 
-	return (totalArea == (area1 + area2 + area3));
+    Fixed tolerance = 0.0001f;
+
+    // Check if the point is on a vertex of the triangle
+    if (area1 < tolerance || area2 < tolerance || area3 < tolerance)
+        return false; // Point is not inside the triangle
+
+    // Check if the point is on an edge of the triangle
+    if ((area1 + area2 + area3) > (totalArea - tolerance)
+		&& (area1 + area2 + area3) < (totalArea + tolerance))
+        return false; // Point is not inside the triangle
+
+    // Check if the point is inside the triangle
+    return true;
 }
+
+/* Examples:
+
+Input: A = (0, 0), B = (10, 30), C = (20, 0), P(10, 15)
+Output: Inside
+
+              B(10,30)
+                / \
+               /   \
+              /     \
+             /   P   \
+            /         \
+     A(0,0) ----------- C(20,0)
+
+Input: A = (0, 0), B = (10, 30), C = (20, 0), P(30, 15)
+Output: Outside
+
+              B(10,30)
+                / \
+               /   \
+              /     \
+             /       \      P
+            /         \
+     A(0,0) ----------- C(20,0)
+
+*/
--- common_core/10_cpp04/module02/ex03/src/main.cpp
+++ common_core/10_cpp04/module02/ex03/src/main.cpp
@@ -6,16 +6,38 @@
 /*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 18:20:39 by etetopat          #+#    #+#             */
-/*   Updated: 2023/07/14 22:23:15 by etetopat         ###   ########.fr       */
+/*   Updated: 2023/07/15 02:19:19 by etetopat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 int	main(void) {
-	if (bsp(Point(0, 0), Point(10, 30), Point(20, 0), Point(10, 15)) == true)
+	Point a(0, 0);
+	Point b(10, 30);
+	Point c(20, 0);
+	Point point(12, 10);
+
+	if (bsp(a, b, c, point) == true)
 		std::cout << "Point is inside the triangle" << std::endl;
-	else
-		std::cout << "Point is outside the triangle" << std::endl;
+	else {
+		Fixed area1 = abs(area(point, a, b));
+		Fixed area2 = abs(area(point, b, c));
+		Fixed area3 = abs(area(point, c, a));
+		Fixed totalArea = abs(area(a, b, c));
+
+		Fixed tolerance = 0.0001f;
+
+	  	// Check if the point is on a vertex or an edge of the triangle
+        if ((area1 + area2 + area3) > (totalArea - tolerance)
+            && (area1 + area2 + area3) < (totalArea + tolerance)) {
+            if (area1 < tolerance || area2 < tolerance || area3 < tolerance)
+                std::cout << "Point is on a vertex of the triangle" << std::endl;
+			else
+                std::cout << "Point is on an edge of the triangle" << std::endl;
+		}
+		else
+			std::cout << "Point is outside the triangle" << std::endl;
+	}
 	return (0);
 }
