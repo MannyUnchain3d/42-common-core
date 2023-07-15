b3345b02ac338ef558f0abb761981c1173c841a4
Manny <etetopat@student.42bangkok.com>
2023-07-15 18:04:07 +0700

    operator== added

--- common_core/10_cpp04/module02/ex03/src/Point.cpp
+++ common_core/10_cpp04/module02/ex03/src/Point.cpp
@@ -3,26 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   Point.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 18:20:36 by etetopat          #+#    #+#             */
-/*   Updated: 2023/07/14 19:14:22 by etetopat         ###   ########.fr       */
+/*   Updated: 2023/07/15 17:01:28 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
+/* ----- CONSTRUCTOR -------------------*/
+
+/* Default Constructor */
 Point::Point(void) : _x(0), _y(0) {
 }
 
+/* Copy Constructor */
 Point::Point(Point const& src) : _x(src.getX()), _y(src.getY()) {
 }
 
+/* Constructor with parameters */
 Point::Point(float const x, float const y) : _x(x), _y(y) {
 }
 
+/* ----- DESTRUCTOR ------------------ */
+
 Point::~Point(void) {
 }
+
+/* ----- OPERATOR OVERLOAD ---------- */
 
 Point&	Point::operator=(Point const& rhs) {
 	if (this != &rhs) {
@@ -31,6 +40,12 @@ Point&	Point::operator=(Point const& rhs) {
 	}
 	return (*this);
 }
+
+bool	Point::operator==(Point const& rhs) const {
+	return (this->_x == rhs._x && this->_y == rhs._y);
+}
+
+/* ----- GETTER ----------------------- */
 
 Fixed	Point::getX(void) const {
 	return (this->_x);
