d4400cb1d7d799960873436d655569a87d916a91
Manny <etetopat@student.42bangkok.com>
2023-07-18 19:43:16 +0700

    syntax changed

--- common_core/10_cpp04/module03/ex00/ClapTrap.cpp
+++ common_core/10_cpp04/module03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 20:48:06 by Manny             #+#    #+#             */
-/*   Updated: 2023/07/16 15:23:51 by Manny            ###   ########.fr       */
+/*   Updated: 2023/07/18 19:37:25 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ ClapTrap::ClapTrap(ClapTrap const& rhs) {
 }
 
 /* Parameter Constructor */
-ClapTrap::ClapTrap(std::string name) : _name(name),
+ClapTrap::ClapTrap(std::string name) : 	_name(name),
 										_hitPoints(10),
 										_energyPoints(10),
 										_attackDamage(0) {
