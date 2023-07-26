beee0e72639aac68ccc33b7b84b5570db6ff711d
Emmanuel Tetopata <etetopat@student.42bangkok.com>
2023-07-27 01:07:24 +0700

    syntax fixed

--- common_core/10_cpp04/module04/ex03/inc/AMateria.hpp
+++ common_core/10_cpp04/module04/ex03/inc/AMateria.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   AMateria.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
+/*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 00:46:18 by Manny             #+#    #+#             */
-/*   Updated: 2023/07/26 02:14:59 by Manny            ###   ########.fr       */
+/*   Updated: 2023/07/27 00:43:05 by etetopat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 # define AMATERIA_HPP
 
 # include <iostream>
+# include "ICharacter.hpp"
 
 class ICharacter;
 
 class AMateria {
 	public:
-		AMateria(void);
 		AMateria(std::string const& type);
 		AMateria(const AMateria& rhs);
 		virtual ~AMateria(void);
 
-		// Assignation operator not needed
+		// No assignation operator overload because this abstract class cannot be instanciated
 
 		std::string const&	getType(void) const;
 		virtual AMateria*	clone(void) const = 0;
 		virtual void		use(ICharacter& target);
 
-	private:
+	protected:
 		std::string	_type;
 };
 
