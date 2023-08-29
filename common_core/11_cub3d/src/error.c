4da6dd5e903f6deb4ae5d05a7a7ac0adae5bb031
Emmanuel Tetopata <etetopat@student.42bangkok.com>
2023-08-29 22:58:04 +0700

    error handling added

---
+++ common_core/11_cub3d/src/error.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 21:02:19 by etetopat          #+#    #+#             */
+/*   Updated: 2023/08/29 21:15:08 by etetopat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	err_msg(char *detail, int *str, int code)
+{
+	ft_putstr_fd("cub3D: Error", 2);
+	if (detail)
+	{
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd(detail, 2);
+	}
+	if (str)
+	{
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd(str, 2);
+	}
+	ft_putstr_fd("\n", 2);
+	return (code);
+}
+
+int	err_msg_nb(int nb, char *str, int code)
+{
+	ft_putstr_fd("cub3D: Error", 2);
+	ft_putstr_fd(nb, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd("\n", 2);
+	return (code);
+}
