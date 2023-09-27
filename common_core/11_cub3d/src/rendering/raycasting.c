d2bffe04b8b7d6e40116da143381861a2fed82b5
Manny <etetopat@student.42bangkok.com>
2023-09-27 21:44:42 +0700

    raycasting in progress

---
+++ common_core/11_cub3d/src/rendering/raycasting.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 21:39:12 by Manny             #+#    #+#             */
+/*   Updated: 2023/09/27 21:42:32 by Manny            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+static void	init_raycasting_info(int x, t_ray *ray, t_player *player)
+{
+	init_ray(ray);
+	ray->camera_x = 2 * x / (double)WIN_WIDTH - 1;
+	ray->dir_x = player->dir_x + player->plane_x * ray->camera_x;
+	ray->dir_y = player->dir_y + player->plane_y * ray->camera_x;
+	ray->map_x = (int)player->pos_x;
+	ray->map_y = (int)player->pos_y;
+	ray->delta_dist_x = fabs(1 / ray->dir_x);
+	ray->delta_dist_y = fabs(1 / ray->dir_y);
+}
