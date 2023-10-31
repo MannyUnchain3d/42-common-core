28770ff65eb2efa1d97cdd0fecf420f0248ece2d
Manny <etetopat@student.42bangkok.com>
2023-10-31 17:26:23 +0700

    syntax fixed

--- common_core/11_cub3d/src/init/init_textures.c
+++ common_core/11_cub3d/src/init/init_textures.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_textures.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etetopat <etetopat@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Manny <etetopat@student.42bangkok.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 20:28:27 by etetopat          #+#    #+#             */
-/*   Updated: 2023/10/24 15:49:08 by etetopat         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:30:00 by Manny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,19 +51,19 @@ void	init_textures(t_data *data)
 	data->textures[EAST] = xpm_to_img(data, data->tex_info.east);
 }
 
-void	init_tex_info(t_tex_info *textures)
+void	init_tex_info(t_tex_info *tex)
 {
-	textures->north = NULL;
-	textures->south = NULL;
-	textures->west = NULL;
-	textures->east = NULL;
-	textures->ceiling = 0;
-	textures->floor = 0;
-	textures->ceiling_hex = 0x000000;
-	textures->floor_hex = 0x000000;
-	textures->size = TEX_SIZE;
-	textures->x = 0;
-	textures->y = 0;
-	textures->pos = 0;
-	textures->step = 0;
+	tex->north = NULL;
+	tex->south = NULL;
+	tex->west = NULL;
+	tex->east = NULL;
+	tex->ceiling = 0;
+	tex->floor = 0;
+	tex->ceiling_hex = 0x000000;
+	tex->floor_hex = 0x000000;
+	tex->size = TEX_SIZE;
+	tex->x = 0;
+	tex->y = 0;
+	tex->pos = 0;
+	tex->step = 0;
 }
